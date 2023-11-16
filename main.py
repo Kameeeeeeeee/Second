c21bcf74fc4f22ef059a6a8c9d15e65425957cf7
Students <abwgd@mail.ru>
2023-11-16 18:51:49 +0300

    My first git-repo

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print('a')
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QColor, QPainter
+from PyQt5 import uic
+import random
+
+
+class Suprematism(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 1000, 1000)
+        self.setWindowTitle('Окно')
+        self.do_paint = True
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        d = random.randint(20, 100)
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        qp.setBrush(QColor(r, g, b))
+        qp.drawEllipse(200 - d // 2, 200 - d // 2, d, d)
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Suprematism()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
